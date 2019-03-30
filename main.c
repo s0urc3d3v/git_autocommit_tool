79f008a797c79d40cccd809f0171b24e234eff08
s0urc3 <github@techredesigned.net>
2019-03-30 15:42:21 -0400

    implement generic git command interface

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <zconf.h>
 
+int issue_git_command(char * git_command[]){
+    int fr = fork();
+
+    if (fr > 0){
+        execv("/bin/git", git_command);
+        return -1;
+    }
+    else if (fr < 0) return -1;
+    else return 1;
+}
 int main() {
-    printf("Hello, World!\n");
+    char * git_commit_command[] = {"git", "commit", "-m", "\"autocommit\""};
+    if (issue_git_command(git_commit_command) < 0) printf("%s", "error");
     return 0;
 }
